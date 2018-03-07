1886975f155f62c562d582c2f923e434e4d55842
yxyxD <a.schade6551@gmail.com>
2018-03-07 11:41:29 +0100

    - finished refactoring launcher

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -2,18 +2,7 @@ package main;
 
 import view.MainFrame;
 
-import java.io.File;
-
 public class Launcher {
-
-
-    /*
-     * @author  yxyxD
-     * @changes
-     *      2018-02-12 (yxyxD)  created
-     * @brief
-     */
-
 
     /*
      * @author  yxyxD
@@ -25,8 +14,6 @@ public class Launcher {
     {
         MainFrame mainFrame = new MainFrame();
         mainFrame.initialize();
-
-        //@todo yxyxD for testing purposes
-        mainFrame.getPdfObjectView().openProject(new File("files/file1.pdf"));
     }
+
 }
